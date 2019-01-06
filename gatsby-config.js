22d71e66dfb3b2560e404e9a25a073fb3b64efba
Josh Barr <josh.barr@xero.com>
2019-01-06 18:03:09 +1300

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,7 @@ module.exports = {
     title: `Josh Barr`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteMetadata: {
-      siteUrl: `http://joshbarr.com`,
-    },
+    siteUrl: `http://joshbarr.com`,
   },
   mapping: {
     "MarkdownRemark.frontmatter.authors": `AuthorYaml`,
