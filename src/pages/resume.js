2a46c88d15c1d4ab4d325fd5a3617f17cb8f04df
Josh Barr <josh.barr@stedi.com>
2023-02-11 16:57:44 +1300

    feat: update website

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -15,7 +15,7 @@ const Resume = () => (
     <div className={styles.cv}>
       <h1>Josh Barr</h1>
 
-      <ul className={styles.nav} style={{marginLeft: '0'}}>
+      <ul className={styles.nav} style={{ marginLeft: '0' }}>
         <li>
           <a href="mailto:josh@joshbarr.com" title="">
             josh@joshbarr.com
@@ -31,65 +31,90 @@ const Resume = () => (
         </li>
       </ul>
 
-      <h2>Lead Architect</h2>
+      <h2>UX Engineer</h2>
 
       <ul>
-        <li>I’m a qualified graphic designer, a technology leader, and guitarist</li>
         <li>
-          I believe in accessible, sensible user experiences
+          I’m a qualified graphic designer, a technology leader, and guitarist
         </li>
-        <li>I'm motivated by collaborating with great people &amp; making an impact at scale</li>
+        <li>I believe in accessible, sensible user experiences</li>
+        <li>
+          I'm motivated by collaborating with great people &amp; making an
+          impact at scale
+        </li>
       </ul>
 
       <h2>Qualifications &amp; skills</h2>
 
       <ul>
         <li>Bachelor of Design (Hons, 1st Class, Massey Scholar)</li>
-        <li>Hands on experience evolving architecture at a $20bn enterprise</li>
         <li>
-          Influencing change at a high level – working with executive stakeholders to
-          set the technical direction for mission critical software
+          Hands-on experience evolving software architecture at a $20bn
+          enterprise
         </li>
-        <li>Leading change to large scale APIs in a compliance-conscious industry</li>
-        <li>Designing solutions with standard AWS building blocks. Lambda, Kinesis, S3, SNS/SQS, ECS, Dynamo and Aurora</li>
+        <li>
+          Crafting user interfaces in React, vanilla JS, and plain old HTML.
+        </li>
+        <li>
+          Influencing change at a high level – working with executive
+          stakeholders to set the technical direction for critical software
+        </li>
+        <li>Working on high-traffic APIs in compliance-conscious industries</li>
+        <li>
+          Designing solutions with standard AWS building blocks. Lambda,
+          Kinesis, S3, SNS/SQS, Dynamo and API Gateway
+        </li>
         <li>Line management – hiring, training and conducting reviews</li>
         <li>
-          Bridging worlds – speaking about technical concerns in plain language
-        </li>
-        <li>
-          Experience as a core contributor to Wagtail, a{' '}
+          Experience as an open-source contributor to Wagtail, a{' '}
           <a href="//github.com/torchbox/wagtail.git">
-            popular open source content management system
+            popular content management system
           </a>
           .
         </li>
-        <li>Developing solutions in TypeScript, Python, C#, Node.js &amp; React</li>
+        <li>Developing in TypeScript, Python, C#, Node.js &amp; React</li>
       </ul>
 
       <h2>The highlights so far</h2>
-      
+
       <ul>
         <li>
-          Transforming software delivery at Xero by leading the adoption of LaunchDarkly. The impact of this has been
-          independently verified by Forrester to have delivered a 245% ROI and several million dollars in productivity gains.   
+          Launching the <a href="https://www.stedi.com/docs">developer docs</a>,
+          design system, and authentication for Stedi
         </li>
         <li>
-          One of three original contributors to Xero's Shell.js, a microfrontend orchestrator allowing teams to 
-          independently release user interfaces. 
+          Transforming software delivery at Xero by leading the adoption of
+          LaunchDarkly for feature management. The impact of this has been
+          independently verified by Forrester to have delivered a 245% ROI and
+          several million dollars in productivity gains.
         </li>
         <li>
-          Modernising Xero's API Gateway, delivering <em>Sign In With Xero</em>
+          Shipping{' '}
+          <a href="https://joshbarr.com/articles/xero-navigation">
+            new navigation
+          </a>{' '}
+          at Xero – "the biggest change to Xero in years"
         </li>
         <li>
-          Four years growing the technical practice at{' '}
+          Modernising Xero's API Gateway, delivering{' '}
+          <a href="https://developer.xero.com/documentation/xero-app-store/app-partner-guides/sign-in/">
+            Sign In With Xero
+          </a>
+        </li>
+        <li>
+          Contributing to Xero's Shell.js, a microfrontend orchestrator allowing
+          teams to independently release user interfaces.
+        </li>
+        <li>
+          Growing the front-end technical practice at{' '}
           <a href="//springload.co.nz/" target="_blank">
             Springload
           </a>
         </li>
         <li>
-          Winning the Information category at the{' '}
-          <a href="https://idealog.co.nz/tech/2016/08/kiwi-companies-bring-home-bacon-internet-awards">
-            2016 ANZIA awards
+          Delivering a rich OpenType editor for{' '}
+          <a href="//klim.co.nz/" target="_blank">
+            Klim Type Foundry
           </a>
         </li>
         <li>
@@ -100,27 +125,26 @@ const Resume = () => (
           core team in Cape Town, 2016
         </li>
         <li>
-          Delivering a rich OpenType editor for{' '}
-          <a href="//klim.co.nz/" target="_blank">
-            Klim Type Foundry
-          </a>
-        </li>
-        <li>
-          Helping 220,000 users engage in democracy with{' '}
-          <a target="_blank" href="http://onthefence.co.nz/">
+          Encouraging 30,000 young people to get out and vote, with{' '}
+          <a
+            target="_blank"
+            href="https://joshbarr.com/articles/getting-young-voters-off-the-fence"
+          >
             On The Fence
           </a>
         </li>
         <li>
-          Working to increase donations to the{' '}
-          <a target="_blank" href="http://redcross.org.nz">
-            New Zealand Red Cross
+          Designing in-flight touchscreen interfaces for{' '}
+          <a href="https://joshbarr.com/articles/air-new-zealand">
+            Air New Zealand
           </a>
         </li>
-        <li>Designing in-flight touchscreen interfaces for Air New Zealand</li>
-        <li>Exhibiting at Neville Brody’s Anti-Design Festival, London 2010</li>
         <li>
-          Best Award – Outstanding, Student Graphic Design for <em>Akzidenz Baby!</em>, 2009
+          Best Award – Outstanding, Student Graphic Design for{' '}
+          <a href="https://joshbarr.com/articles/akzidenz-baby">
+            Akzidenz Baby!
+          </a>
+          , 2009
         </li>
       </ul>
 
@@ -128,96 +152,154 @@ const Resume = () => (
 
       <h3>Employment History</h3>
 
-      <h4>Lead Portfolio Architect, Xero</h4>
-      <p className={styles.date}>Dec 2019 – Present</p>
+      <h4>UX Engineer, Stedi</h4>
+      <p className={styles.date}>Apr 2021 – Feb 2023</p>
       <ul>
         <li>
-          Coordinated across the Ecosystem group to design the architecture for Xero's transactional app store
+          Established Stedi's Design Language System, for shared UI components
+          across all applications
         </li>
         <li>
-          Led a time-critical regulatory compliance project to shift WorkflowMax and Xero Practice Manager behind our OAuth2 Gateway
+          In collaboration with Stedi's Recruiter, worked to make our front-end
+          hiring process more inclusive.
         </li>
         <li>
-          Subject matter expert on APIs, consulted by teams across the company
+          Migrated Stedi's Developer Docs and API Reference from a third-party
+          host to Next.js, including a custom renderer for OpenAPI
+          Specifications.
         </li>
         <li>
-          Led the migration of developer.xero.com to Gatsby (GraphQL) &amp; Contentful CMS. 
+          Built a CLI and dynamo-backed API for publishing versioned OpenAPI
+          specifications from teams' repositories
         </li>
         <li>
-          Passionate contributor to Xero's global architecture principles and architecture strategy
+          Consolidated our frontend web applications – previously deployed on
+          subdomains – behind a single domain
+        </li>
+        <li>
+          Key contributor and code owner for Stedi's Authentication library.
+          Worked with the AWS team to reduce Amplify's bundle size.
+        </li>
+        <li>
+          Front-end ops: configured e2e tests with Playwright and Cypress, built
+          deployment pipelines in Github Actions, ensured security compliance
+          with custom reporting tooling on top of AWS SecurityHub,
+          Infrastructure-as-code with AWS CDK.
         </li>
       </ul>
 
-      <h4 className={styles.printBreakBefore}>Solution Architect, Xero</h4>
+      <h4>Lead Portfolio Architect, Xero</h4>
+      <p className={styles.date}>Dec 2019 – Mar 2021</p>
+      <ul>
+        <li>
+          Coordinated across the 80-person Ecosystem group to design the
+          architecture for Xero's transactional{' '}
+          <a href="https://apps.xero.com/nz">app store</a>, including contract
+          management and partner onboarding
+        </li>
+        <li>
+          Wrote architectural whitepapers for senior leadership outlining
+          important team and technology changes required for the business to
+          meet its goals.
+        </li>
+        <li>
+          Led a time-critical regulatory compliance project to shift WorkflowMax
+          and Xero Practice Manager behind our OAuth2 Gateway
+        </li>
+        <li>
+          Led the modernisation of developer.xero.com, from a bespoke
+          application to open-source standard building blocks.
+        </li>
+        <li>
+          Core contributor to Xero's global architecture principles and
+          architecture strategy
+        </li>
+        <li>
+          Upheld and improved the Ecosystem's security posture, through
+          collaboration with Xero Security and external Security Advisors
+        </li>
+      </ul>
+
+      <h4>Solution Architect, Xero</h4>
       <p className={styles.date}>July 2018 – Nov 2019</p>
       <ul>
         <li>
-          Technical mentor and advisor for key initiatives in the Xero Ecosystem:
+          Technical mentor and advisor for key initiatives in the Xero
+          Ecosystem:
         </li>
         <li>
-          Modernised our API Gateway, delivered Single Sign On, Informed Consent, OAuth 2
+          Modernised our API Gateway, delivered Single Sign On, Informed
+          Consent, OAuth 2
         </li>
         <li>
-          Guided the creation of a new app marketplace for Xero, migrating to Contentful CMS
+          Guided the creation of a new app marketplace for Xero, migrating from
+          static content to a dynamic CMS, enabling a signed in experience.
         </li>
         <li>
           Led the adoption of LaunchDarkly for feature management across Xero
         </li>
         <li>
-          Laid the foundations for secure single page applications with Xero's microfrontends solution – Shell.js
+          Laid the foundations for secure single page applications with Xero's
+          microfrontend solution – Shell.js
         </li>
         <li>
-          Facilitator for API Community of Practice, building consensus on the right way to build APIs.
+          Facilitator for API Community of Practice, building consensus on the
+          right way to build APIs.
         </li>
       </ul>
 
       <h4>Frontend Architect, Xero</h4>
-    
+
       <p className={styles.date}>Nov 2016 - July 2018</p>
       <ul>
         <li>
-          Architected and implemented new navigation for Xero's 18 customer-facing
-          web applications. Heralded as the "biggest change to Xero in years".
+          Architected and implemented new navigation for Xero's 18
+          customer-facing web applications.
         </li>
         <li>
-          Created libraries for dev teams to implement OpenId
-          Connect authentication.
+          Created libraries for dev teams to implement OpenId Connect
+          authentication in their web applications.
         </li>
         <li>
-          Replaced bespoke frontend build tool with Webpack, saving 100
-          hours/month in CI server time.
+          Replaced a bespoke frontend build tool with Webpack, saving 100
+          hours/month in CI server time. This changed enabled almost ~1m lines
+          of frontend JS to be statically analysed for the first time.
         </li>
         <li>
-          Technical lead for a pilot programme aiming to internationalise Xero's web applications 
+          Technical lead for a pilot programme aiming to internationalise Xero's
+          web applications
         </li>
         <li>
-          Advocate for containerisation and pipelines as code, using Jenkins, Docker and ECS.
+          Advocate for containerisation and pipelines as code, using Jenkins,
+          Docker and ECS.
         </li>
       </ul>
 
       <h4>Technical Director, Springload</h4>
       <p className={styles.date}>Nov 2015 - Nov 2016</p>
       <p>
-        Together with one other technical director, I was responsible for running the technical
-        practice at Springload. Over 2014-2016, we transformed Springload's
-        technical culture, initiating continuous integration, continuous
-        deployment, and unit testing practices. We standardised environments
-        and moved our client workloads on to AWS.
+        Together with one other technical director, I was responsible for
+        running the technical practice at Springload. Over 2014-2016, we
+        transformed Springload's technical culture, initiating continuous
+        integration, continuous deployment, and unit testing practices. We
+        standardised environments and moved our client workloads on to AWS.
       </p>
 
       <h4>Designer, GoodWorld</h4>
       <p className={styles.date}>Aug 2014 - Feb 2015</p>
       <p>
         Working with the Wellington-based development team and Washington-based
-        business team, I contributed to GoodWorld's user experience, leading to a successful funding
-        round and Fast Company's{' '}
+        business team, I contributed to GoodWorld's user experience, leading to
+        a successful funding round and Fast Company's{' '}
         <a href="http://www.fastcompany.com/most-innovative-companies/sectors/social-good">
           most innovative non-profits
         </a>{' '}
         to watch in 2016.
       </p>
 
-      <h4>Front-end director, Springload</h4>
+      <h4 className={styles.printBreakBefore}>
+        Front-end director, Springload
+      </h4>
       <p className={styles.date}>Dec 2013 - Nov 2015</p>
       <p>
         Responsible for growing and maintaining quality of the Springload
@@ -231,21 +313,20 @@ const Resume = () => (
       <h4>Senior front-end developer, Springload</h4>
       <p className={styles.date}>Jan 2013 - Dec 2013</p>
       <p>
-        I worked to standardise Springload’s front-end practice and clear
-        technical debt. I introduced a formal methodology for CSS and tools to
-        expedite development. Key projects include developing the Forsyth Barr
-        Investment Insights app and contributing to the frontend of Kiwibank
-        Home Hunter.
+        I worked to expand Springload’s front-end practice and clear technical
+        debt. I introduced a formal methodology for CSS (BEM) and command-line
+        tooling such as Gulp &amp; Webpack. Key projects include developing the
+        Forsyth Barr Investment Insights app and contributing to the frontend of
+        Kiwibank Home Hunter.
       </p>
 
       <h4>Designer, Springload</h4>
       <p className={styles.date}>Nov 2011 - Jan 2013</p>
       <p>
-        Responsible for user experience and design on my allocated
-        projects, under the direction of Andy Hovey. Highlights include
-        redesigning the New Zealand Red Cross website, launching the first
-        responsive bank homepage for Kiwibank, as well as projects for RealMe
-        and NZ Post.
+        Responsible for user experience and design on my allocated projects,
+        under the direction of Andy Hovey. Highlights include redesigning the
+        New Zealand Red Cross website, launching the first responsive bank
+        homepage for Kiwibank, as well as projects for RealMe and NZ Post.
       </p>
 
       <h4>Interactive Designer, Designworks Wellington</h4>
@@ -257,7 +338,7 @@ const Resume = () => (
         design and illustration.
       </p>
 
-      <h4 className={styles.printBreakBefore}>Visiting Tutor, Massey University</h4>
+      <h4>Visiting Tutor, Massey University</h4>
       <p className={styles.date}>February 2011 - June 2011 (fixed term)</p>
       <p>
         Responsibilities included tutoring 300-level graphic design, planning
@@ -286,13 +367,12 @@ const Resume = () => (
         explored the relationship between digital and physical publication.
         During this time I worked as a freelance web developer.
       </p>
-
       <hr />
       <p>
         <em>References available on request</em>
       </p>
       <p className="noprint">
-        <a href="joshbarr-lead-architect-2020.pdf" title="">
+        <a href="/static/resume-josh-barr-2023.pdf" title="Josh Barr – 2023">
           Download this page (PDF)
         </a>
       </p>
